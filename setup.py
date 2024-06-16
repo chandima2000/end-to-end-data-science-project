771df6ef8e57cd90fc2d6e0a2b995e8be56c1bcc
chandima2000 <hychandima2000@gmail.com>
2024-06-17 00:14:19 +0530

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="data-science-project",
+    version="0.0.1",
+    author="chandima",
+    author_email="hychandima2000@gmail.com",
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
