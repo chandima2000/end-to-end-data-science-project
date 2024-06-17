88ffb6af36ac1c3387a7ba0ae029863bef3f28f4
chandima2000 <hychandima2000@gmail.com>
2024-06-17 14:11:54 +0530

    create logger functionality

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+## This log file is uniquely named based on the current date and time to
+## ensure that each run of the program generates a separate log file.
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"  #Generating a Unique Log File Name
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE) #creates the full path for the log file.
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+)
