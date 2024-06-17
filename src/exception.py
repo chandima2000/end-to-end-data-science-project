3cee81ecb9c2bc6889f82442eb726e5447f3181d
chandima2000 <hychandima2000@gmail.com>
2024-06-17 14:44:25 +0530

    Test custom exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 ## create custom exception
@@ -6,7 +7,7 @@ import sys
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()  ## error_detail.exc_info() retrieves the exception information: 
  ## the exception type, exception value, and traceback object.
- 
+
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
@@ -21,3 +22,14 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+
+### test the exception handling
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero error")
+#         raise CustomException(e,sys)
