4265fb849e5652ade973d91076aef89134d103dd
chandima2000 <hychandima2000@gmail.com>
2024-06-17 12:57:36 +0530

    create custom exception

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,23 @@
 import sys
 
+## create custom exception
 
 
-
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()  ## error_detail.exc_info() retrieves the exception information: 
+ ## the exception type, exception value, and traceback object.
  
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
