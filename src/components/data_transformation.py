986d1155951286ea583b7bc00d4d733f861f70b0
chandima2000 <hychandima2000@gmail.com>
2024-06-21 01:05:02 +0530

    add set of import statements

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,13 @@
 ### How to handle one hot encoding, label encoding
+### Do feature Engineering, Data cleaning
+
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer 
+from dataclasses import dataclass
+from sklearn.impute import SimpleImputer 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
