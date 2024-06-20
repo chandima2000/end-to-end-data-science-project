32b48c54cbd68fb87d04d9c67047a7b1d68e8437
chandima2000 <hychandima2000@gmail.com>
2024-06-21 00:00:00 +0530

    create DataIngestion class

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -21,3 +21,43 @@ class DataIngestionConfig:
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            # Read the dataset from csv file. This can be from database, api
+            df=pd.read_csv('notebook\data\students.csv')
+            logging.info("Data loaded successfully as data frame")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            # Get the data from the data source and insert it into the csv file.
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info("create the raw dataset from the data frame")
+
+            train_set, test_set=train_test_split(df,test_size=0.2,random_state=42)
+            logging.info("Train, Test split initiated")
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            logging.info("create tain dataset successfully")
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("create test dataset successfully")
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            logging.info("Data Ingestion failed!")
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
