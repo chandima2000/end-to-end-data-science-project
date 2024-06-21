c68e578113a52850cf20743c351f5a6fba64a54e
chandima2000 <hychandima2000@gmail.com>
2024-06-21 14:55:06 +0530

    create "initiate_data_transformation" function

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,6 +12,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -21,7 +22,7 @@ class DataTransformation:
      def __init__(self):
         self.data_transformation_config=DataTransformationConfig() # create object
 
-    ## Create all pickle files
+    ## Data Preprocessing
      def get_data_transformer_object(self):
         try:
             numerical_columns = ["writing_score", "reading_score"]
@@ -72,4 +73,55 @@ class DataTransformation:
         except CustomException as e:
             logging.info("DataTransformation is failed!")
             raise CustomException(e, sys)
+    
+     ## Starting the data transformation
+     def initiate_data_transformation(self,train_path,test_path):   ### train_path,test_path coming from the data_ingestion.py
 
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read train and test data is completed.")
+
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(
+                "Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+
+            # This function is coming from the utils file.
+            # Use to save .pkl files
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj # This is what we want to save
+
+            )
+            logging.info("Saved preprocessing object.")
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
