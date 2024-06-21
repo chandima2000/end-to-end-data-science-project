011a6861e31ac3861d18b607c7e24b8cdb2841f5
chandima2000 <hychandima2000@gmail.com>
2024-06-21 19:55:11 +0530

    print the r2_score_test

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 # This class is responsible for where the test data should save,
@@ -67,4 +69,10 @@ if __name__=="__main__":
 
     # Execute data transformation
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
