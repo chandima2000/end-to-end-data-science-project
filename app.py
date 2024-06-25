b10043a43143b53cd4f67f4a5586a9312b3659c4
chandima2000 <hychandima2000@gmail.com>
2024-06-25 22:38:17 +0530

    Make a route for prediction page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 # This gives the the entrypoint for the flask app
 application=Flask(__name__)
@@ -14,3 +15,34 @@ app = application
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+@app.route("/predict",methods=['GET','POST'])
+def predict():
+    if request.method=='GET':
+        return render_template("home.html")
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+
+        #Get the Dataframe
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_pipeline=PredictPipeline()
+        prediction=predict_pipeline.predict(pred_df)
+
+        return render_template("home.html",results=prediction[0])
+
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1",debug=True)  # App is run on port 5000 by default
+
